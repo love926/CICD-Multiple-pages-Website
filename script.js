f7d5716c475992a4dada777952cff48cf823daca
Ali Tahir <you@example.com>
2026-05-07 00:39:10 +0500

    First Version

---
+++ script.js
@@ -0,0 +1,12 @@
+function orderCoffee(){
+    alert("☕ Your coffee order has been added!");
+}
+
+function payNow(){
+    alert("💳 Payment Successful! Enjoy your coffee ☕");
+}
+
+function loginUser(){
+    alert("✅ Login Successful");
+}
+
